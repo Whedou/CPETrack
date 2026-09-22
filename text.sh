a48aceb7070953813d35ebe13b4d7efcb432fd13
Whedou <98176096+Whedou@users.noreply.github.com>
2026-09-22 15:52:10 +0800

    Delete text.sh

--- text.sh
+++
@@ -1,7 +0,0 @@
-python tools/build_lasher_text_cache.py \
-  --seq-list \
-  /data/pudata/LasHeR/TrainingSet/train/trainingsetList.txt \
-  --output /data/wsk/project/pixeltext/cache/lashertrain_text_features.pt \
-  --provider open_clip \
-  --open-clip-model ViT-B-32 \
-  --open-clip-pretrained /data/wsk/project/pixeltext/pretrained/open_clip_pytorch_model.bin
